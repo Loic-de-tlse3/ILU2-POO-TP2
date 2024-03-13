5a90d700c314a19a8faab46339b0bc08de28e8d8
loic-de-tlse3 <loic.francois1@univ-tlse3>
2024-03-13 11:56:26 +0100

    4 - PrendreEtal en cours

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -11,8 +11,7 @@ public class BoundaryEmmenager {
 
 	public void emmenager(String nomVisiteur) {
 		if (controlEmmenager.isHabitant(nomVisiteur)) {
-			System.out.println(
-					"Mais vous êtes déjà un habitant du village !");
+			System.out.println("Mais vous êtes déjà un habitant du village !");
 		} else {
 			StringBuilder question = new StringBuilder();
 			question.append("Êtes-vous :\n");
@@ -27,12 +26,17 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					StringBuilder bienvenue = new StringBuilder();
+					bienvenue.append("Bienvenue villageois " + nomVisiteur);
+					System.out.println(bienvenue.toString());
+					StringBuilder questionForceGaulois = new StringBuilder();
+					questionForceGaulois.append("Quelle est votre force ?");
+					int forceGaulois = Clavier.entrerEntier(questionForceGaulois.toString());
+					controlEmmenager.ajouterGaulois(nomVisiteur, forceGaulois);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +44,25 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		StringBuilder bienvenue = new StringBuilder();
+		bienvenue.append("Bienvenue druide " + nomVisiteur);
+		System.out.println(bienvenue.toString());
+		StringBuilder questionForceDruide = new StringBuilder();
+		questionForceDruide.append("Quelle est votre force ?");
+		int forceDruide = Clavier.entrerEntier(questionForceDruide.toString());
+		int effetPotionMax = -1;
+		int effetPotionMin = 0;
+		do {
+			StringBuilder questionForcePotionMin = new StringBuilder();
+			questionForcePotionMin.append("Quelle est la force de potion la plus faible que vous produisez ?");
+			effetPotionMin = Clavier.entrerEntier(questionForcePotionMin.toString());
+			StringBuilder questionForcePotionMax = new StringBuilder();
+			questionForcePotionMax.append("Quelle est la force de potion la plus forte que vous produisez ?");
+			effetPotionMax = Clavier.entrerEntier(questionForcePotionMax.toString());
+			if (effetPotionMax < effetPotionMin) {
+				System.out.println("attention Druide, vous vous �tes tromp� entre le minimum et le maximum");
+			}
+		} while (effetPotionMax < effetPotionMin);
+		controlEmmenager.ajouterDruide(nomVisiteur, forceDruide, effetPotionMin, effetPotionMax);
 	}
 }
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -3,6 +3,7 @@ package frontiere;
 import java.util.Scanner;
 
 import controleur.ControlPrendreEtal;
+import villagegaulois.Village;
 
 public class BoundaryPrendreEtal {
 	private ControlPrendreEtal controlPrendreEtal;
@@ -13,10 +14,23 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (! nomVendeurConnu) {
+			System.out.println("Je suis désolée " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+		} else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal");
+			boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (! etalDisponible) {
+				System.out.println("Désolée " +  nomVendeur + " je n'ai plus d'étal qui ne soit pas déjà occupé.");
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait il me reste un étal pour vous !");
+		StringBuilder questionRenseignement = new StringBuilder();
+		questionRenseignement.append("Il me faudrait quelques renseignements:\nQuel produit souhaitez-vous vendre ?");
 	}
 }
